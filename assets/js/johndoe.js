9779eadc9e5eccd51075a54019d682810aeda852
Ronal280300 <rosepa2803@gmail.com>
2024-09-11 16:59:34 -0600

    Commit 03

--- assets/js/johndoe.js
+++ assets/js/johndoe.js
@@ -1,67 +1,62 @@
-/*!
-=========================================================
-* JohnDoe Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+  $(".navbar .nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
 
-        if (this.hash !== "") {
+      var hash = this.hash;
 
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 });
 
 // protfolio filters
-$(window).on("load", function() {
-    var t = $(".portfolio-container");
-    t.isotope({
-        filter: ".new",
-        animationOptions: {
+$(window).on("load", function () {
+  var t = $(".portfolio-container");
+  t.isotope({
+    filter: ".new",
+    animationOptions: {
+      duration: 750,
+      easing: "linear",
+      queue: !1,
+    },
+  }),
+    $(".filters a").click(function () {
+      $(".filters .active").removeClass("active"), $(this).addClass("active");
+      var i = $(this).attr("data-filter");
+      return (
+        t.isotope({
+          filter: i,
+          animationOptions: {
             duration: 750,
             easing: "linear",
-            queue: !1
-        }
-    }), $(".filters a").click(function() {
-        $(".filters .active").removeClass("active"), $(this).addClass("active");
-        var i = $(this).attr("data-filter");
-        return t.isotope({
-            filter: i,
-            animationOptions: {
-                duration: 750,
-                easing: "linear",
-                queue: !1
-            }
-        }), !1
+            queue: !1,
+          },
+        }),
+        !1
+      );
     });
 });
 
-
-$(document).ready(function() {
-  $(".accordion-header").click(function() {
+$(document).ready(function () {
+  $(".accordion-header").click(function () {
     var parentItem = $(this).parent();
 
     // Cerrar cualquier otro acordeón abierto
-    $(".accordion-item").not(parentItem).removeClass("active").find(".accordion-content").slideUp();
+    $(".accordion-item")
+      .not(parentItem)
+      .removeClass("active")
+      .find(".accordion-content")
+      .slideUp();
     $(".accordion-item").not(parentItem).find(".accordion-icon").text("+");
 
     // Abrir/cerrar el acordeón actual
@@ -71,10 +66,9 @@ $(document).ready(function() {
     } else {
       parentItem.addClass("active").find(".accordion-content").slideDown({
         duration: 500,
-        easing: "swing"
+        easing: "swing",
       });
       $(this).find(".accordion-icon").text("×");
     }
   });
 });
-
